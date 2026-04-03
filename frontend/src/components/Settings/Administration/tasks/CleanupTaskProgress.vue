518372f1f584b83237311ecc0919a69377f3b2ea
Georges-Antoine Assi <contact@me.gantoine.com>
2026-04-03 14:23:24 -0400

    ru trunk fmt

--- frontend/src/components/Settings/Administration/tasks/CleanupTaskProgress.vue
+++ frontend/src/components/Settings/Administration/tasks/CleanupTaskProgress.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
-import type { OrphanedResourcesCleanupStats, CleanupTaskStatusResponse } from "@/__generated__";
+import type {
+  OrphanedResourcesCleanupStats,
+  CleanupTaskStatusResponse,
+} from "@/__generated__";
 
 const { t } = useI18n();
 
